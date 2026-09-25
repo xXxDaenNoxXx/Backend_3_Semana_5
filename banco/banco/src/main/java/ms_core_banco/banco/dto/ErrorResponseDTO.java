fcf736d79e4e44ad8588a83cee6b8eed193039fc
DaenNo1 <da.ericesn@duocuc.cl>
2026-09-25 15:54:46 -0300

    Delete banco/banco directory

--- banco/banco/src/main/java/ms_core_banco/banco/dto/ErrorResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package ms_core_banco.banco.dto;
-
-public record ErrorResponseDTO(String mensaje) {
-}
--- banco/banco/src/main/java/ms_core_banco/banco/repository/CuentaInteresRepository.java
+++
@@ -1,8 +0,0 @@
-package ms_core_banco.banco.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ms_core_banco.banco.model.CuentaInteres;
-
-public interface CuentaInteresRepository extends JpaRepository<CuentaInteres, Long> {
-}
